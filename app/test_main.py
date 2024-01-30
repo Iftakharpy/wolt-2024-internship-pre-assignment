86280c13432cc5839283e088f2e5158ca9ee4e43
Iftakhar Husan <iftakharn19@gmail.com>
2024-01-30 06:39:37 +0200

    Add test files

---
+++ app/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from http import HTTPStatus
+from .main import app
+
+
+client = TestClient(app)
+
+
+def test_docs_redirect():
+    response = client.get("/", follow_redirects=False)
+    assert response.status_code == HTTPStatus.PERMANENT_REDIRECT
+    assert response.headers["location"] == "/docs"
