13fc7876bceef3a0eeefdbcc260ba5330ea08ab1
Iftakhar Husan <iftakharn19@gmail.com>
2024-01-31 22:23:23 +0200

    clean up test cases

--- app/tests/delivery_fee/test_data_parsing.py
+++ app/tests/delivery_fee/test_data_parsing.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from http import HTTPStatus
 from app.main import app
@@ -6,38 +7,40 @@ from app.main import app
 client = TestClient(app)
 
 
-def test__cart_value__less_than_0():
-    response = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": -1,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+edge_cases_for_numeric_fields = [-1, -10, 100.5, None,
+                                 [1, 2, 3], [], {}, {"a": 1, "b": 2}, "invalid"]
 
 
-def test__cart_value__invalid_data_types():
-    # Try string
+#########################################################################################
+# Test cart_value field
+#########################################################################################
+
+def test__cart_value_with_valid_data():
+    # Try with valid data
     string_res = client.post("/api/delivery/calculate_delivery_fee/",
                              json={
-                                 "cart_value": "invalid",
+                                 "cart_value": 0,
+                                 "delivery_distance": 0,
+                                 "number_of_items": 0,
+                                 "time": "2024-01-15T13:00:00Z"
+                             })
+    assert string_res.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize("cart_value", edge_cases_for_numeric_fields)
+def test__cart_value__with_invalid_data_types(cart_value):
+    # Try with each of the cart_value values in the list above
+    string_res = client.post("/api/delivery/calculate_delivery_fee/",
+                             json={
+                                 "cart_value": cart_value,
                                  "delivery_distance": 0,
                                  "number_of_items": 0,
                                  "time": "2024-01-15T13:00:00Z"
                              })
     assert string_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    # Try None/null
-    none_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": None,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert none_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
+def test__cart_value__edge_case_boolean():
     # Try boolean
     bool_res = client.post("/api/delivery/calculate_delivery_fee/",
                            json={
@@ -50,59 +53,37 @@ def test__cart_value__invalid_data_types():
     # True as 1 and False as 0
     assert bool_res.status_code == HTTPStatus.OK
 
-    # Try list
-    list_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": [1, 2, 3],
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert list_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    # Try dict
-    dict_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": {"a": 1, "b": 2},
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert dict_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+#########################################################################################
+# Test delivery_distance field
+#########################################################################################
 
-
-def test__delivery_distance__less_than_0():
-    response = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": -10,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-
-def test__delivery_distance__invalid_data_types():
-    # Try string
+def test__delivery_distance_with_valid_data():
+    # Try with valid data
     string_res = client.post("/api/delivery/calculate_delivery_fee/",
                              json={
                                  "cart_value": 0,
-                                 "delivery_distance": "invalid",
+                                 "delivery_distance": 0,
+                                 "number_of_items": 0,
+                                 "time": "2024-01-15T13:00:00Z"
+                             })
+    assert string_res.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize("delivery_distance", edge_cases_for_numeric_fields)
+def test__delivery_distance__with_invalid_data_types(delivery_distance):
+    # Try with each of the delivery_distance values in the list above
+    string_res = client.post("/api/delivery/calculate_delivery_fee/",
+                             json={
+                                 "cart_value": 0,
+                                 "delivery_distance": delivery_distance,
                                  "number_of_items": 0,
                                  "time": "2024-01-15T13:00:00Z"
                              })
     assert string_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    # Try None/null
-    none_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": None,
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert none_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
+def test__delivery_distance__edge_case_boolean():
     # Try boolean
     bool_res = client.post("/api/delivery/calculate_delivery_fee/",
                            json={
@@ -115,150 +96,74 @@ def test__delivery_distance__invalid_data_types():
     # True as 1 and False as 0
     assert bool_res.status_code == HTTPStatus.OK
 
-    # Try list
-    list_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": [0, 1, 3],
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert list_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    # Try dict
-    dict_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": {"a": 1, "b": 2},
-                               "number_of_items": 0,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert dict_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+#########################################################################################
+# Test number_of_items field
+#########################################################################################
+
+def test__number_of_items_with_valid_data():
+    # Try with valid data
+    string_res = client.post("/api/delivery/calculate_delivery_fee/",
+                             json={
+                                 "cart_value": 0,
+                                 "delivery_distance": 0,
+                                 "number_of_items": 0,
+                                 "time": "2024-01-15T13:00:00Z"
+                             })
+    assert string_res.status_code == HTTPStatus.OK
 
 
-def test__number_of_items__less_than_0():
+@pytest.mark.parametrize("number_of_items", edge_cases_for_numeric_fields)
+def test__number_of_items__with_invalid_data_types(number_of_items):
+    # Try with each of the number_of_items values in the list above
     response = client.post("/api/delivery/calculate_delivery_fee/",
                            json={
                                "cart_value": 0,
                                "delivery_distance": 0,
-                               "number_of_items": -10,
+                               "number_of_items": number_of_items,
                                "time": "2024-01-15T13:00:00Z"
                            })
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test__number_of_items__invalid_data_types():
-    # Try string
-    string_res = client.post("/api/delivery/calculate_delivery_fee/",
-                             json={
-                                 "cart_value": 0,
-                                 "delivery_distance": 0,
-                                 "number_of_items": "invalid",
-                                 "time": "2024-01-15T13:00:00Z"
-                             })
-    assert string_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    # Try None/null
-    none_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": None,
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert none_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
+def test__number_of_items__edge_case_boolean():
     # Try boolean
     bool_res = client.post("/api/delivery/calculate_delivery_fee/",
                            json={
                                "cart_value": 0,
                                "delivery_distance": 0,
-                               "number_of_items": True,
+                               "number_of_items": False,
                                "time": "2024-01-15T13:00:00Z"
                            })
     # Expecting ok since it is quite common to interpret
     # True as 1 and False as 0
     assert bool_res.status_code == HTTPStatus.OK
 
-    # Try list
-    list_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": [0, 1, 3],
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert list_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-    # Try dict
-    dict_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": {"a": 1, "b": 2},
-                               "time": "2024-01-15T13:00:00Z"
-                           })
-    assert dict_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+#########################################################################################
+# Test time field
+#########################################################################################
 
-
-def test__time__invalid_format():
-    response = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": "invalid"
-                           })
-    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-
-def test__time__invalid_data_types():
-    # Try string
+def test__time_with_valid_data():
+    # Try with valid data
     string_res = client.post("/api/delivery/calculate_delivery_fee/",
                              json={
                                  "cart_value": 0,
                                  "delivery_distance": 0,
                                  "number_of_items": 0,
-                                 "time": "invalid"
+                                 "time": "2024-01-15T13:00:00Z"
+                             })
+    assert string_res.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize("time", [edge_cases_for_numeric_fields]+[True])
+def test__time__invalid_data_types(time):
+    # Try with each of the time values in the list above
+    string_res = client.post("/api/delivery/calculate_delivery_fee/",
+                             json={
+                                 "cart_value": 0,
+                                 "delivery_distance": 0,
+                                 "number_of_items": 0,
+                                 "time": time
                              })
     assert string_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    # Try None/null
-    none_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": None
-                           })
-    assert none_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    # Try boolean
-    bool_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": True
-                           })
-    assert bool_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    # Try list
-    list_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": [0, 1, 3]
-                           })
-    assert list_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    # Try dict
-    dict_res = client.post("/api/delivery/calculate_delivery_fee/",
-                           json={
-                               "cart_value": 0,
-                               "delivery_distance": 0,
-                               "number_of_items": 0,
-                               "time": {"a": 1, "b": 2}
-                           })
-    assert dict_res.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
