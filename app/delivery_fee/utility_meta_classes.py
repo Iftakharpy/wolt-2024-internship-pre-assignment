d354da8876ec93cd591b50af93d084bfdff9aa3f
Iftakhar Husan <iftakharn19@gmail.com>
2024-01-31 09:49:35 +0200

    Remove experimental source code

--- app/delivery_fee/utility_meta_classes.py
+++ app/delivery_fee/utility_meta_classes.py
@@ -56,19 +56,3 @@ class SingletonMeta(type):
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
-
-
-class Singleton(metaclass=ThreadSafeSingletonMeta):
-    value: str = None
-    """
-    We'll use this property to prove that our Singleton really works.
-    """
-
-    def __init__(self, value: str) -> None:
-        self.value = value
-
-    def some_business_logic(self):
-        """
-        Finally, any singleton should define some business logic, which can be
-        executed on its instance.
-        """
